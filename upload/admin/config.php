a9f9e1e57db5dd795a6c5f77a25c2916fcf78e38
Muhammad Fuad <m.fuad_90@yahoo.com>
2017-09-12 11:23:04 +0800

    change config

--- upload/admin/config.php
+++ upload/admin/config.php
@@ -1,13 +1,10 @@
 <?php
 // HTTP
+/*
 define('HTTP_SERVER', 'http://localhost/~fuad/my/upload/admin/');
 define('HTTP_CATALOG', 'http://localhost/~fuad/my/upload/');
-
-// HTTPS
 define('HTTPS_SERVER', 'http://localhost/~fuad/my/upload/admin/');
 define('HTTPS_CATALOG', 'http://localhost/~fuad/my/upload/');
-
-// DIR
 define('DIR_APPLICATION', '/Users/fuad/Sites/my/upload/admin/');
 define('DIR_SYSTEM', '/Users/fuad/Sites/my/upload/system/');
 define('DIR_IMAGE', '/Users/fuad/Sites/my/upload/image/');
@@ -19,7 +16,28 @@ define('DIR_DOWNLOAD', '/Users/fuad/Sites/my/upload/system/storage/download/');
 define('DIR_LOGS', '/Users/fuad/Sites/my/upload/system/storage/logs/');
 define('DIR_MODIFICATION', '/Users/fuad/Sites/my/upload/system/storage/modification/');
 define('DIR_UPLOAD', '/Users/fuad/Sites/my/upload/system/storage/upload/');
-define('DIR_CATALOG', '/Users/fuad/Sites/my/upload/catalog/');
+define('DIR_CATALOG', '/Users/fuad/Sites/my/upload/catalog/'); */
+
+
+define('HTTP_SERVER', 'http://asiaebuy.com/my/upload/admin/');
+define('HTTP_CATALOG', 'http://asiaebuy.com/my/upload/');
+define('HTTPS_SERVER', 'http://asiaebuy.com/my/upload/admin/');
+define('HTTPS_CATALOG', 'http://asiaebuy.com/my/upload/');
+define('DIR_APPLICATION', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/admin/');
+define('DIR_SYSTEM', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/system/');
+define('DIR_IMAGE', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/image/');
+define('DIR_LANGUAGE', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/admin/language/');
+define('DIR_TEMPLATE', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/admin/view/template/');
+define('DIR_CONFIG', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/system/config/');
+define('DIR_CACHE', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/system/storage/download/');
+define('DIR_LOGS', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/catalog/');
+
+
+
 
 // DB
 /*
--- upload/config.php
+++ upload/config.php
@@ -1,15 +1,11 @@
 <?php
-// HTTP
+
+/*
 define('HTTP_SERVER', 'http://localhost/~fuad/my/upload/');
-
-// HTTPS
 define('HTTPS_SERVER', 'http://localhost/~fuad/my/upload/');
-
-// DIR
 define('DIR_APPLICATION', '/Users/fuad/Sites/my/upload/catalog/');
 define('DIR_SYSTEM', '/Users/fuad/Sites/my/upload/system/');
 define('DIR_IMAGE', '/Users/fuad/Sites/my/upload/image/');
-
 define('DIR_LANGUAGE', '/Users/fuad/Sites/my/upload/catalog/language/');
 define('DIR_TEMPLATE', '/Users/fuad/Sites/my/upload/catalog/view/theme/');
 define('DIR_CONFIG', '/Users/fuad/Sites/my/upload/system/config/');
@@ -17,7 +13,23 @@ define('DIR_CACHE', '/Users/fuad/Sites/my/upload/system/storage/cache/');
 define('DIR_DOWNLOAD', '/Users/fuad/Sites/my/upload/system/storage/download/');
 define('DIR_LOGS', '/Users/fuad/Sites/my/upload/system/storage/logs/');
 define('DIR_MODIFICATION', '/Users/fuad/Sites/my/upload/system/storage/modification/');
-define('DIR_UPLOAD', '/Users/fuad/Sites/my/upload/system/storage/upload/');
+define('DIR_UPLOAD', '/Users/fuad/Sites/my/upload/system/storage/upload/'); */
+
+define('HTTP_SERVER', 'http://asiaebuy.com/my/upload/');
+define('HTTPS_SERVER', 'http://asiaebuy.com/my/upload/');
+define('DIR_APPLICATION', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/catalog/');
+define('DIR_SYSTEM', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/system/');
+define('DIR_IMAGE', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/image/');
+define('DIR_LANGUAGE', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/system/config/');
+define('DIR_CACHE', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/system/storage/download/');
+define('DIR_LOGS', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/vhosts/asiaebuy.com/httpdocs/my/upload/system/storage/upload/');
+
+
 
 // DB
 /*
